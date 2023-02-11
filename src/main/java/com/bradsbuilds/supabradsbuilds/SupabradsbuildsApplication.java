efad9ae13cc1501aa18c2a06c20adadadf7508b6
Brad <Bradkharris@gmail.com>
2023-02-10 17:35:58 -0700

    initial commit

---
+++ src/main/java/com/bradsbuilds/supabradsbuilds/SupabradsbuildsApplication.java
@@ -0,0 +1,13 @@
+package com.bradsbuilds.supabradsbuilds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupabradsbuildsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupabradsbuildsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bradsbuilds/supabradsbuilds/SupabradsbuildsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bradsbuilds.supabradsbuilds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupabradsbuildsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
